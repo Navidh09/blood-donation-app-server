b0025d801595a00f310557364783b8927859f227
Navid Hasan <navid.nyon@gmail.com>
2025-05-18 23:51:04 +0600

    deployed to vercel

--- index.js
+++ index.js
@@ -226,10 +226,9 @@ async function run() {
       res.send(requests);
     });
 
-    // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
